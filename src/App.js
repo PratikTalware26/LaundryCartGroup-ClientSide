08a572d3f646292999d24457fa1454ea18d0ff1f
Prasanta Mandal <pm863390@gmail.com>
2023-03-02 17:03:52 +0530

    added routing to the frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-
-
-
-
-
+import React, { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Createorder from "./components/createOrder/createorder/createorder";
+import CreateOrderLanding from "./components/createOrder/landingPage/landingPage";
+import PastOrder from "./components/pastOrder/pastOrderList/pastOrderPage";
+//mport SignInPage from "./components/SignIn/SignInPage";
+export const tokenstorage = createContext();
 
 function App() {
+  const [token, settoken] = useState(null);
   return (
     <div className="App">
-    
+      <tokenstorage.Provider value={[token, settoken]}>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/" element={<SignInPage/>}/> */}
+            <Route path="/home" element={<CreateOrderLanding />} />
+            <Route path="/createorder" element={<Createorder />} />
+            <Route path="/pastorders" element={<PastOrder />} />
+          </Routes>
+        </BrowserRouter>
+      </tokenstorage.Provider>
     </div>
   );
 }
