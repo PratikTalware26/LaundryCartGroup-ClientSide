df496d062c43695c1a553df512792dc81e02dff7
Pratik <pratiktalware26@gmail.com>
2023-03-02 20:28:50 +0530

    Register and sign in page added also routing part and validation part added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import RegisterPage from './Components/SignIn/RegisterPage';
 import SignInPage from './Components/SignIn/SignInPage';
 
 function App() {
   return (
     <div className="App">
-      <SignInPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignInPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
