e9e04fb011a69e3c4f8f2ea4305f8b433ab59040
Pratik <pratiktalware26@gmail.com>
2023-03-02 09:41:17 +0530

    Sign In UI page completed, Navbar footer added

---
+++ src/Components/SignIn/SignInPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import SignIn from './SignIn'
+
+const SignInPage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <SignIn/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default SignInPage
